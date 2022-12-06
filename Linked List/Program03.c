8f94471568506973b3dafdebf7e9763b7b82ba97
Nusrat Farzana Choudhury <105939501+Nusrat-Farzana-Choudhury@users.noreply.github.com>
2022-12-06 12:24:54 +0600

    Singly linked list creation (Using function)

---
+++ Linked List/Program03.c
@@ -0,0 +1,62 @@
+/*Problem- Create a singly linked list to store data for ID, name & CGPA of a student in a node (Using function)*/
+
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node NODE; //self-referential structure
+
+struct node{
+	int id;
+	char name[20];
+    float cgpa;
+	NODE *next;	
+};
+NODE *head=NULL, *next=NULL, *temp=NULL, *current=NULL;
+int num, i=1;
+void create(){
+	/*Dynamic creation of linked list*/
+	
+	printf("Enter the number of nodes you wanna insert\t: ");
+	scanf("%d",&num);
+	printf("\n\n");
+	
+	while(num--){
+		current= (NODE*)malloc(sizeof(NODE));           //memory allocation for a node
+		printf("We have got a node\t\t: %d\n",current);
+		printf("Enter the ID in node-%d\t\t: ",i);
+		scanf("%d",&current->id);                       //data 1
+		printf("Enter the name in node-%d\t: ",i);
+		scanf("%s",&current->name);                     //data 2
+		printf("Enter the CGPA in node-%d\t: ",i);
+		scanf("%f",&current->cgpa);                     //data 3
+		i++;                                            //updating counter of node 
+		fflush(stdin);
+		current->next=NULL;
+		
+		/*Linking nodes here*/
+		if(head==NULL){
+			head=temp=current;                         //identifying head of the node
+		}
+		else{
+			temp->next=current;
+			temp=current;
+		}
+		printf("\n\n");
+	}
+}
+void show(){
+	/*Printing the list*/
+	printf("The status of the linked list\t:\n");
+	temp = head;
+	while(temp != 0){
+		printf("[%d]----->[%d][%s][%.2f]",temp,temp->id,temp->name,temp->cgpa);
+		temp = temp->next;
+	}
+	printf("[0]");
+}
+
+int main(){
+	create();
+	show();
+	
+	return 0;
+}
